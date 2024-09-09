15ae34998040cc28b13528a6d8d76f3218a58db1
Dulguun0314 <Dulguunbuyannemeh0314@gmail.com>
2024-09-09 17:59:29 +0800

    hsadh

--- frontend/src/app/orderHistory/page.tsx
+++ frontend/src/app/orderHistory/page.tsx
@@ -28,26 +28,26 @@ const orderHistory = () => {
 
   const histories: Histories[] = [
     {
-      src: "/chunky.png",
-      text: "Chunky Glyph Tee",
+      src: "/smallChunky.png",
+      text: "smallChunky Glyph Tee",
       piece: "1",
       price: "120’000₮",
     },
     {
-      src: "/chunky.png",
-      text: "Chunky Glyph Tee",
+      src: "/smallChunky.png",
+      text: "smallChunky Glyph Tee",
       piece: "1",
       price: "120’000₮",
     },
     {
-      src: "/chunky.png",
-      text: "Chunky Glyph Tee",
+      src: "/smallChunky.png",
+      text: "smallChunky Glyph Tee",
       piece: "1",
       price: "120’000₮",
     },
     {
-      src: "/chunky.png",
-      text: "Chunky Glyph Tee",
+      src: "/smallChunky.png",
+      text: "smallChunky Glyph Tee",
       piece: "1",
       price: "120’000₮",
     },
@@ -60,7 +60,7 @@ const orderHistory = () => {
   }
   return (
     <div className="flex justify-center bg-[#F7F7F8] ">
-      <div className="container justify-center h-screen flex ">
+      <div className="container justify-center h-screen flex my-32 gap-5 ">
         <div className="flex-1">
           {paths.map((path, index) => (
             <Link key={index} href={path.path}>
@@ -78,35 +78,46 @@ const orderHistory = () => {
         </div>
         <div className="grid w-full h-fit">
           <p className="text-[18px] font-bold">Захиалгын түүх</p>
-          <div className="w-full border"></div>
-          <div>
-            <p className="text-[16px] font-bold">2024-09-03 15:34</p>
-            <button className="bg-[#2563EB ] mx-[10px] my-2 ">
-              <p>хүргэлтэнд гарсан</p>
-            </button>
-            <div onClick={() => setSide(!side)}>
-              {side ? <FaChevronUp /> : <FaChevronDown />}
+          <div className="w-full border my-6"></div>
+          <div className="bg-white rounded-2xl  px-8 py-6">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <p className="text-[16px] font-bold">2024-09-03 15:34</p>
+                <button className="bg-[#2563EB] text-white rounded-xl px-[10px] py-1 ">
+                  <p>хүргэлтэнд гарсан</p>
+                </button>
+              </div>
+              <div onClick={() => setSide(!side)}>
+                {side ? <FaChevronUp /> : <FaChevronDown />}
+              </div>
             </div>
             <div className={`w-full ${side ? "visible" : "hidden"}`}>
               {histories.map((history, index) => (
-                <div key={index} className="flex">
-                  <div className="relative w-9 h-9">
-                    <Image src={history.src} alt="hi" fill />
-                  </div>
-                  <div>
-                    <p>{history.text}</p>
+                <div key={index} className="flex  justify-between">
+                  <div className=" flex items-center">
+                    <div className="relative w-12 h-12 grid gap-2">
+                      <Image
+                        src={history.src}
+                        alt="hi"
+                        fill
+                        className="rounded-md  "
+                      />
+                    </div>
                     <div>
-                      {history.piece} x {history.price}
+                      <p>{history.text}</p>
+                      <div>
+                        {history.piece} x {history.price}
+                      </div>
                     </div>
                   </div>
                   <div>{history.price}</div>
                 </div>
               ))}
-            </div>
-            <div className="w-full border-dashed "></div>
-            <div>
-              <p>Үнийн дүн:</p>
-              <p>480’000₮</p>
+              <div className="w-full border-dashed "></div>
+              <div>
+                <p>Үнийн дүн:</p>
+                <p>480’000₮</p>
+              </div>
             </div>
           </div>
         </div>
